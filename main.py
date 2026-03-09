31d586ab70899f113ee11a278666832036f5d12a
Poola Tejaswi <poolatejaswi2021@gmail.com>
2026-03-09 16:18:17 +0530

    Enable CORS in FASTAPI

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import pickle
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # allow all websites
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Load model and vectorizer
 model = pickle.load(open("model.pkl","rb"))
